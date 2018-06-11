854b6be38e8a22b4439366dfe66f3aa449eeb179
robsonklima <robsonklima@gmail.com>
2018-06-11 00:53:36 -0300

    Remove Buttom on Requirements

--- src/pages/requirements/requirements.ts
+++ src/pages/requirements/requirements.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 import { RequirementAddPage } from './requirement-add';
 
@@ -17,10 +17,25 @@ export class RequirementsPage {
   ]
 
   constructor(
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private toastCtrl: ToastController
   ) {}
 
   public onLoadAddRequirement() {
     this.navCtrl.push(RequirementAddPage);
+  }
+
+  public onRemoveRequirement(requirement: any) {
+    this.exibirToast(`${requirement} removed successfully.`);
+  }
+
+  private exibirToast(mensagem: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const toast = this.toastCtrl.create({
+        message: mensagem, duration: 3000, position: 'bottom'
+      });
+
+      resolve(toast.present());
+    });
   }
 }
